49cd718b2556780f5324f2e42c1b5bb3ee221b87
otzoy97 <otzoy97@hotmail.com>
2019-04-11 18:05:36 -0600

    Se agrego la funcionalidad de "Nuevo" permitiendo agregar dinámicamente nuevas pestañas. Ahora la ventana y todos sus componentes hijos son "resizables" y adaptables

--- PROYECTO/Main.Designer.cs
+++ PROYECTO/Main.Designer.cs
@@ -28,21 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.itemAbrir = new System.Windows.Forms.ToolStripMenuItem();
-            this.itemNuevo = new System.Windows.Forms.ToolStripMenuItem();
+            this.ItemAbrir = new System.Windows.Forms.ToolStripMenuItem();
+            this.ItemNuevo = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.itemGuardar = new System.Windows.Forms.ToolStripMenuItem();
-            this.itemGuardarComo = new System.Windows.Forms.ToolStripMenuItem();
+            this.ItemGuardar = new System.Windows.Forms.ToolStripMenuItem();
+            this.ItemGuardarComo = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-            this.itemSalir = new System.Windows.Forms.ToolStripMenuItem();
+            this.ItemSalir = new System.Windows.Forms.ToolStripMenuItem();
             this.herramientasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.itemErrores = new System.Windows.Forms.ToolStripMenuItem();
-            this.itemAST = new System.Windows.Forms.ToolStripMenuItem();
+            this.ItemErrores = new System.Windows.Forms.ToolStripMenuItem();
+            this.ItemAST = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
-            this.itemCompilar = new System.Windows.Forms.ToolStripMenuItem();
+            this.ItemCompilar = new System.Windows.Forms.ToolStripMenuItem();
+            this.TabOutput = new System.Windows.Forms.TabControl();
+            this.TabConsola = new System.Windows.Forms.TabPage();
+            this.TabVariable = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.TabInput = new System.Windows.Forms.TabControl();
             this.menuStrip1.SuspendLayout();
+            this.TabOutput.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -53,118 +60,189 @@
             this.herramientasToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(978, 28);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // archivoToolStripMenuItem
             // 
             this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.itemAbrir,
-            this.itemNuevo,
+            this.ItemAbrir,
+            this.ItemNuevo,
             this.toolStripSeparator1,
-            this.itemGuardar,
-            this.itemGuardarComo,
+            this.ItemGuardar,
+            this.ItemGuardarComo,
             this.toolStripSeparator2,
-            this.itemSalir});
+            this.ItemSalir});
             this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
             this.archivoToolStripMenuItem.Size = new System.Drawing.Size(71, 24);
             this.archivoToolStripMenuItem.Text = "Archivo";
             // 
-            // itemAbrir
+            // ItemAbrir
             // 
-            this.itemAbrir.Name = "itemAbrir";
-            this.itemAbrir.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.itemAbrir.Size = new System.Drawing.Size(293, 26);
-            this.itemAbrir.Text = "Abrir...";
+            this.ItemAbrir.Name = "ItemAbrir";
+            this.ItemAbrir.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.ItemAbrir.Size = new System.Drawing.Size(293, 26);
+            this.ItemAbrir.Text = "Abrir...";
+            this.ItemAbrir.Click += new System.EventHandler(this.ItemAbrir_Click);
             // 
-            // itemNuevo
+            // ItemNuevo
             // 
-            this.itemNuevo.Name = "itemNuevo";
-            this.itemNuevo.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
-            this.itemNuevo.Size = new System.Drawing.Size(293, 26);
-            this.itemNuevo.Text = "Nuevo";
+            this.ItemNuevo.Name = "ItemNuevo";
+            this.ItemNuevo.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.ItemNuevo.Size = new System.Drawing.Size(293, 26);
+            this.ItemNuevo.Text = "Nuevo";
+            this.ItemNuevo.Click += new System.EventHandler(this.ItemNuevo_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
             this.toolStripSeparator1.Size = new System.Drawing.Size(290, 6);
             // 
-            // itemGuardar
+            // ItemGuardar
             // 
-            this.itemGuardar.Name = "itemGuardar";
-            this.itemGuardar.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.itemGuardar.Size = new System.Drawing.Size(293, 26);
-            this.itemGuardar.Text = "Guardar";
+            this.ItemGuardar.Name = "ItemGuardar";
+            this.ItemGuardar.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.ItemGuardar.Size = new System.Drawing.Size(293, 26);
+            this.ItemGuardar.Text = "Guardar";
+            this.ItemGuardar.Click += new System.EventHandler(this.ItemGuardar_Click);
             // 
-            // itemGuardarComo
+            // ItemGuardarComo
             // 
-            this.itemGuardarComo.Name = "itemGuardarComo";
-            this.itemGuardarComo.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            this.ItemGuardarComo.Name = "ItemGuardarComo";
+            this.ItemGuardarComo.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
             | System.Windows.Forms.Keys.S)));
-            this.itemGuardarComo.Size = new System.Drawing.Size(293, 26);
-            this.itemGuardarComo.Text = "Guardar como...";
+            this.ItemGuardarComo.Size = new System.Drawing.Size(293, 26);
+            this.ItemGuardarComo.Text = "Guardar como...";
+            this.ItemGuardarComo.Click += new System.EventHandler(this.ItemGuardarComo_Click);
             // 
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
             this.toolStripSeparator2.Size = new System.Drawing.Size(290, 6);
             // 
-            // itemSalir
+            // ItemSalir
             // 
-            this.itemSalir.Name = "itemSalir";
-            this.itemSalir.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
-            this.itemSalir.Size = new System.Drawing.Size(293, 26);
-            this.itemSalir.Text = "Salir";
+            this.ItemSalir.Name = "ItemSalir";
+            this.ItemSalir.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.ItemSalir.Size = new System.Drawing.Size(293, 26);
+            this.ItemSalir.Text = "Salir";
             // 
             // herramientasToolStripMenuItem
             // 
             this.herramientasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.itemErrores,
-            this.itemAST,
+            this.ItemErrores,
+            this.ItemAST,
             this.toolStripSeparator3,
-            this.itemCompilar});
+            this.ItemCompilar});
             this.herramientasToolStripMenuItem.Name = "herramientasToolStripMenuItem";
             this.herramientasToolStripMenuItem.Size = new System.Drawing.Size(110, 24);
             this.herramientasToolStripMenuItem.Text = "Herramientas";
             // 
-            // itemErrores
+            // ItemErrores
             // 
-            this.itemErrores.Name = "itemErrores";
-            this.itemErrores.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
-            this.itemErrores.Size = new System.Drawing.Size(257, 26);
-            this.itemErrores.Text = "Reporte de errores";
+            this.ItemErrores.Name = "ItemErrores";
+            this.ItemErrores.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
+            this.ItemErrores.Size = new System.Drawing.Size(257, 26);
+            this.ItemErrores.Text = "Reporte de errores";
             // 
-            // itemAST
+            // ItemAST
             // 
-            this.itemAST.Name = "itemAST";
-            this.itemAST.ShortcutKeys = System.Windows.Forms.Keys.F5;
-            this.itemAST.Size = new System.Drawing.Size(257, 26);
-            this.itemAST.Text = "Generar AST";
+            this.ItemAST.Name = "ItemAST";
+            this.ItemAST.ShortcutKeys = System.Windows.Forms.Keys.F5;
+            this.ItemAST.Size = new System.Drawing.Size(257, 26);
+            this.ItemAST.Text = "Generar AST";
             // 
             // toolStripSeparator3
             // 
             this.toolStripSeparator3.Name = "toolStripSeparator3";
             this.toolStripSeparator3.Size = new System.Drawing.Size(254, 6);
             // 
-            // itemCompilar
+            // ItemCompilar
             // 
-            this.itemCompilar.Name = "itemCompilar";
-            this.itemCompilar.ShortcutKeys = System.Windows.Forms.Keys.F6;
-            this.itemCompilar.Size = new System.Drawing.Size(257, 26);
-            this.itemCompilar.Text = "Compilar";
+            this.ItemCompilar.Name = "ItemCompilar";
+            this.ItemCompilar.ShortcutKeys = System.Windows.Forms.Keys.F6;
+            this.ItemCompilar.Size = new System.Drawing.Size(257, 26);
+            this.ItemCompilar.Text = "Compilar";
+            // 
+            // TabOutput
+            // 
+            this.TabOutput.Controls.Add(this.TabConsola);
+            this.TabOutput.Controls.Add(this.TabVariable);
+            this.TabOutput.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.TabOutput.Location = new System.Drawing.Point(0, 463);
+            this.TabOutput.MinimumSize = new System.Drawing.Size(847, 216);
+            this.TabOutput.Name = "TabOutput";
+            this.TabOutput.Padding = new System.Drawing.Point(5, 5);
+            this.TabOutput.SelectedIndex = 0;
+            this.TabOutput.Size = new System.Drawing.Size(978, 216);
+            this.TabOutput.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.TabOutput.TabIndex = 0;
+            // 
+            // TabConsola
+            // 
+            this.TabConsola.Location = new System.Drawing.Point(4, 29);
+            this.TabConsola.Name = "TabConsola";
+            this.TabConsola.Padding = new System.Windows.Forms.Padding(3);
+            this.TabConsola.Size = new System.Drawing.Size(970, 183);
+            this.TabConsola.TabIndex = 0;
+            this.TabConsola.Text = "Consola";
+            this.TabConsola.UseVisualStyleBackColor = true;
+            // 
+            // TabVariable
+            // 
+            this.TabVariable.Location = new System.Drawing.Point(4, 29);
+            this.TabVariable.Name = "TabVariable";
+            this.TabVariable.Padding = new System.Windows.Forms.Padding(3);
+            this.TabVariable.Size = new System.Drawing.Size(839, 183);
+            this.TabVariable.TabIndex = 1;
+            this.TabVariable.Text = "Variables";
+            this.TabVariable.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 31);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(0, 0);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // TabInput
+            // 
+            this.TabInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TabInput.Location = new System.Drawing.Point(0, 28);
+            this.TabInput.MaximumSize = new System.Drawing.Size(99999, 99999);
+            this.TabInput.MinimumSize = new System.Drawing.Size(840, 400);
+            this.TabInput.Name = "TabInput";
+            this.TabInput.SelectedIndex = 0;
+            this.TabInput.Size = new System.Drawing.Size(978, 435);
+            this.TabInput.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.TabInput.TabIndex = 3;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(978, 679);
+            this.Controls.Add(this.TabInput);
+            this.Controls.Add(this.TabOutput);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(865, 700);
             this.Name = "Main";
-            this.Text = "[OLC1] - PROYECTO 2";
+            this.Text = "Interpreter";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.TabOutput.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -174,18 +252,23 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem itemAbrir;
-        private System.Windows.Forms.ToolStripMenuItem itemNuevo;
+        private System.Windows.Forms.ToolStripMenuItem ItemAbrir;
+        private System.Windows.Forms.ToolStripMenuItem ItemNuevo;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
-        private System.Windows.Forms.ToolStripMenuItem itemGuardar;
-        private System.Windows.Forms.ToolStripMenuItem itemGuardarComo;
+        private System.Windows.Forms.ToolStripMenuItem ItemGuardar;
+        private System.Windows.Forms.ToolStripMenuItem ItemGuardarComo;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
-        private System.Windows.Forms.ToolStripMenuItem itemSalir;
+        private System.Windows.Forms.ToolStripMenuItem ItemSalir;
         private System.Windows.Forms.ToolStripMenuItem herramientasToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem itemErrores;
-        private System.Windows.Forms.ToolStripMenuItem itemAST;
+        private System.Windows.Forms.ToolStripMenuItem ItemErrores;
+        private System.Windows.Forms.ToolStripMenuItem ItemAST;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
-        private System.Windows.Forms.ToolStripMenuItem itemCompilar;
+        private System.Windows.Forms.ToolStripMenuItem ItemCompilar;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TabControl TabOutput;
+        private System.Windows.Forms.TabPage TabConsola;
+        private System.Windows.Forms.TabPage TabVariable;
+        private System.Windows.Forms.TabControl TabInput;
     }
 }
 
--- PROYECTO/Main.cs
+++ PROYECTO/Main.cs
@@ -1,28 +1,139 @@
-﻿using System;
+﻿using FastColoredTextBoxNS;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace PROYECTO
 {
     public partial class Main : Form
     {
+        //Servirá para llevar un mejor control de las tab generadas
+        private LinkedList<TabPage> tabPages;
+        private int tabContador;
+        //Servirá para guardar o abrir un archivo
+        private SaveFileDialog saveDialog;
+        private OpenFileDialog openDialog;
         public Main()
         {
-            InitializeComponent();
+            InitializeComponent();           
+            openDialog = new OpenFileDialog
+            {
+                //Establece el directorio inicial
+                InitialDirectory = "C:\\Users\\otzoy\\Desktop",
+                //Especifica qué tipo de archivos se aceptarán
+                Filter = "Documento de Text (*.txt) | *.txt",
+                FilterIndex = 0,
+                //Indica que deberá recordar el directorio anterior
+                RestoreDirectory = true,
+                //Evita que se seleccione más de un archivo a la vez
+                Multiselect = false
+            };
+            saveDialog = new SaveFileDialog
+            {
+                //Establece el directorio inicial
+                InitialDirectory = "C:\\Users\\otzoy\\Desktop",
+                //Se crea y agrega un filtro para los archivos
+                Filter = "Documento de Texto (*.txt) |*.txt",
+                FilterIndex = 0,
+                //Deja el directorio en el directorio en el que se cerro la 
+                //última vez que se abrio el cuadro de dialogo durante 
+                //el tiempo de ejecuacion
+                RestoreDirectory = true,
+            };
+            tabPages = new LinkedList<TabPage>();
+            tabContador = 0;
         }
 
-        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        private void ItemAbrir_Click(object sender, EventArgs e)
         {
+            //Hace visible el dialogo
+            //Si se presiona OK se selecciona un archivo y ejecuta la instrucción dentro del if
+            if (openDialog.ShowDialog()==DialogResult.OK && !String.IsNullOrEmpty(openDialog.FileName))
+            {
+                //Crea un nuevo tab y agrega el contenido al txtbox, a menos que la dirección 
+                //del fileName ya exista en la lista de Nombres
 
+            }
+        }
+        private void ItemNuevo_Click(object sender, EventArgs e)
+        {
+            //Tabpage y FastTextBox
+            TabPage tabPage = new TabPage
+            {
+                Text = "Untitled"+tabContador,
+                Name = "TabPage" + tabContador++
+            };
+            FastColoredTextBox textBox = new FastColoredTextBox
+            {
+                Name = "TextBox",
+                ShowScrollBars = true,
+                Dock = DockStyle.Fill
+                /*Height = 223,
+                Width = 625*/
+            };
+            tabPage.Controls.Add(textBox);
+            tabPages.AddLast(tabPage);
+            this.TabInput.Controls.Add(tabPage);
+            //this.TabInput.Controls.Add(tabPage.Controls.Add(textBox));
         }
 
-        private void erroresLéxicosToolStripMenuItem_Click(object sender, EventArgs e)
+        private void ItemGuardarComo_Click(object sender, EventArgs e)
+        {
+            //this.SaveFileLike();
+        }
+
+        private void ItemGuardar_Click(object sender, EventArgs e)
+        {
+            //Detecta si el tab actual posee alguna entrada en la lista de archivos
+            //si no posee , procede a un guardar como, si lo posee solo guarda
+        }
+
+        private void SaveFileLike()
+        { 
+            //Despliega el dialogo para guardar un archivo
+            if (saveDialog.ShowDialog()==DialogResult.OK)
+            {
+                //Detecta el tab al cual se le tiene focus y procede a guardar el archivo
+                //SaveFile(saveDialog.FileName,);
+            }
+        }
+        
+        private void SaveFile(String direccionArchivo, String contenidoArchivo, String extensionArchivo)
+        {
+            if (!String.IsNullOrEmpty(direccionArchivo))
+            {
+                StreamWriter outputFile = null;
+                try
+                {
+                    using (outputFile = new StreamWriter(direccionArchivo + "." + extensionArchivo))
+                    {
+                        outputFile.Write(contenidoArchivo);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(this, "Error " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                finally
+                {
+                    //Cierra el archivo
+                    try
+                    {
+                        if (outputFile != null)
+                        {
+                            outputFile.Close();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(this, "Error " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+        }
+
+        private void TabInput_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
