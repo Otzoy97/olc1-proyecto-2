a58a3aa9ab1aaa512fbcf0c5317edb440a03c401
otzoy97 <otzoy97@hotmail.com>
2019-04-17 23:39:23 -0600

    Se agregaron otro terminales, como palabras reservadas, aún faltan algunas otras.

--- PROYECTO/Gramatica/Sintactico.cs
+++ PROYECTO/Gramatica/Sintactico.cs
@@ -37,7 +37,6 @@ namespace PROYECTO.Gramatica
             var Cadena = new StringLiteral("tkSTR", "\"");
             /*-------- CARACTER --------*/
             var Caracter = new StringLiteral("tkCHAR", "'", StringOptions.IsChar);
-
             /*-------- PUNTUACION --------*/
             var SEMICOLON = ToTerm(";", "SEMICOLON");
             var COMMA = ToTerm(",", "COMMA");
@@ -69,20 +68,67 @@ namespace PROYECTO.Gramatica
             var INCREMENTO = ToTerm("++", "INCREMENTO");
             var DECREMENTO = ToTerm("--", "DECREMENTO");
             var NOT = ToTerm("!","NOT");
+            /*-------- RESERVADAS --------*/
+            /*-------- DEFINICION DE TIPOS --------*/
+            var INT = ToTerm("int", "INT");
+            var CHAR = ToTerm("char", "CHAR");
+            var STRING = ToTerm("string", "STRING");
+            var DOUBLE = ToTerm("double", "DOUBLE");
+            var BOOL = ToTerm("bool", "BOOL");
+            var VOID = ToTerm("void", "VOID");
+            var ARRAY = ToTerm("array", "ARRAY");
+            /*-------- CLASES --------*/
+            var PUBLICO = ToTerm("publico", "PUBLICO");
+            var PRIVADO = ToTerm("privado", "PRIVADO");
+            var OVERRIDE = ToTerm("override", "OVERRIDE");
+            var IMPORTAR = ToTerm("importar", "IMPORTAR");
+            var NEW = ToTerm("new", "NEW");
+            var MAIN = ToTerm("main", "MAIN");
+            var RETURN = ToTerm("return", "RETURN");
+            /*-------- NATIVA --------*/
+            var PRINT = ToTerm("print", "PRINT");
+            var SHOW = ToTerm("show", "SHOW");
+            /*-------- CONDICIONALES Y LOOPS --------*/
+            var IF = ToTerm("if", "IF");
+            var ELSE = ToTerm("else", "ELSE");
+            var FOR = ToTerm("for", "FOR");
+            var REPEAT = ToTerm("repeat", "REPEAT");
+            var WHILE = ToTerm("while", "WHILE");
+            var COMPROBAR = ToTerm("comprobar", "COMPROBAR");
+            var CASO = ToTerm("caso", "CASO");
+            var DEFECTO = ToTerm("default", "DEFECTO");
+            var SALIR = ToTerm("salir", "SALIR");
+            var HACER = ToTerm("hacer", "HACER");
+            var MIENTRAS = ToTerm("mientras", "MIENTRAS");
+            var CONTINUAR = ToTerm("continuar", "CONTINUAR");
             #endregion
 
             #region No terminales
+            var INICIO = new NonTerminal("INICIO");
+            var OPER = new NonTerminal("OPER");
             #endregion
 
             #region Producciones
+
             #endregion
 
             #region Preferencias
+            /*-------- INICIO --------*/
+            this.Root = INICIO;
             /*-------- COMENTARIOS IGNORADOS --------*/
             NonGrammarTerminals.Add(LineComment);
             NonGrammarTerminals.Add(BlockComment);
             /*-------- PUNTUACIÓN Y AGRUPACIÓN --------*/
             MarkPunctuation(SEMICOLON, COLON, DOT, COMMA, PARIZQ, PARDER, LLVIZQ, LLVDER, CORIZQ, CORDER);
+            /*-------- PUNTUACIÓN Y AGRUPACIÓN --------*/
+            RegisterOperators(0, Associativity.Left, OR);
+            RegisterOperators(1, Associativity.Left, AND);
+            RegisterOperators(2, Associativity.Left, IGUAL, DIFERENTE, MAYOR, MAYOR_IGUAL, MENOR, MENOR_IGUAL);
+            RegisterOperators(3, Associativity.Left, NOT);
+            RegisterOperators(4, Associativity.Left, MAS, MENOS);
+            RegisterOperators(5, Associativity.Left, POR, DIVISION);
+            RegisterOperators(6, Associativity.Left, POTENCIA);
+            RegisterOperators(7, Associativity.Left, PARIZQ, PARDER);
             #endregion
         }
     }
