695aee4111efc1ae6af6e9e16364ca67faee0ec3
otzoy97 <otzoy97@hotmail.com>
2019-04-19 00:04:37 -0600

    Se agregaron reglas de producción para las operaciones binarias y unarias,  así como para la declaración y asignación de variables y arreglos

--- PROYECTO/Gramatica/Sintactico.cs
+++ PROYECTO/Gramatica/Sintactico.cs
@@ -12,7 +12,7 @@ namespace PROYECTO.Gramatica
     {
         public Sintactico() : base(caseSensitive: false)
         {
-            #region REGEX
+            #region Regex
             #endregion
 
             #region Terminales
@@ -20,19 +20,8 @@ namespace PROYECTO.Gramatica
             var LineComment = new CommentTerminal("LineComment", ">>", "\n", "\r\n");
             /*-------- COMENTARIOS EN BLOQUE --------*/
             var BlockComment = new CommentTerminal("BlockComment", "<-", "->");
-            /*-------- IDENTIFICADORES --------*/
-            var Identificador = new IdentifierTerminal("tkID");
-            var Variable = new IdentifierTerminal("tkVAR");
-            /*-------- ENTEROS --------*/
-            var Entero = new NumberLiteral("tkINT", NumberOptions.AllowSign | NumberOptions.IntOnly | NumberOptions.NoDotAfterInt);
-            /*-------- DECIMALES --------*/
-            var Decimal = new NumberLiteral("tkREAL", NumberOptions.AllowSign);
-            /*-------- BOOLEANOS --------*/
-            var Booleano = new ConstantTerminal("tkBOOL");
-            Booleano.Add("true", true);
-            Booleano.Add("verdadero", true);
-            Booleano.Add("falso", false);
-            Booleano.Add("false", false);
+            /*-------- NUMERO --------*/
+            var Real = new NumberLiteral("tkREAL", NumberOptions.AllowSign);
             /*-------- CADENA --------*/
             var Cadena = new StringLiteral("tkSTR", "\"");
             /*-------- CARACTER --------*/
@@ -63,11 +52,11 @@ namespace PROYECTO.Gramatica
             var MENOR_IGUAL = ToTerm("<=", "MENOR_IGUAL");
             var MAYOR = ToTerm(">", "MAYOR");
             var MENOR = ToTerm("<", "MENOR");
-            var ASIGNACION = ToTerm("=","ASIGNACION");
+            var ASIGNACION = ToTerm("=", "ASIGNACION");
             /*-------- UNARIOS --------*/
             var INCREMENTO = ToTerm("++", "INCREMENTO");
             var DECREMENTO = ToTerm("--", "DECREMENTO");
-            var NOT = ToTerm("!","NOT");
+            var NOT = ToTerm("!", "NOT");
             /*-------- RESERVADAS --------*/
             /*-------- DEFINICION DE TIPOS --------*/
             var INT = ToTerm("int", "INT");
@@ -101,15 +90,89 @@ namespace PROYECTO.Gramatica
             var HACER = ToTerm("hacer", "HACER");
             var MIENTRAS = ToTerm("mientras", "MIENTRAS");
             var CONTINUAR = ToTerm("continuar", "CONTINUAR");
+            /*-------- IDENTIFICADORES --------*/
+            var Identificador = new IdentifierTerminal("tkID");
+            var Variable = new IdentifierTerminal("tkVAR");
             #endregion
 
             #region No terminales
             var INICIO = new NonTerminal("INICIO");
+            /*Contiene los tipos de datos utilizados*/
+            var DATATYPE = new NonTerminal("DATATYPE");
+            /*Contiene una lista de variables*/
+            var VARLIST = new NonTerminal("VARLIST");
+            /*Declara las variables normales*/
+            var DECLARACION_SIMPLE = new NonTerminal("DECLARACION_SIMPLE");
+            /*Declara arreglos*/
+            var DECLARACION_ARRAY = new NonTerminal("DECLARACION_ARRAY");
+            /*Asigna variables normales*/
+            var ASIGNACION_SIMPLE = new NonTerminal("ASIGNACION_SIMPLE");
+            /*Asigna arreglos*/
+            var ASIGNACION_ARRAY = new NonTerminal("ASIGNACION_SIMPLE");
+            /*Establece la dimensión de los arreglos*/
+            var DIMENSION = new NonTerminal("SET_DIMENSION");
+            var DIMENSION_1 = new NonTerminal("SET_DIMENSION");
+            var DIMENSION_2 = new NonTerminal("SET_DIMENSION");
+            var DIMENSION_3 = new NonTerminal("SET_DIMENSION");
+            /*Establece el contenido de los arreglos al declararse*/
+            var SET_CONTENT = new NonTerminal("SET_CONTENT");
+            /*Específica la forma de las operaciones*/
             var OPER = new NonTerminal("OPER");
             #endregion
 
             #region Producciones
+            INICIO.Rule = OPER;
 
+            #region DECLARACION, ASIGNACION Y GET DE VARIABLES Y ARREGLOS
+
+            DATATYPE.Rule = INT | CHAR | STRING | DOUBLE | BOOL;
+            VARLIST.Rule = VARLIST + COMMA + Variable | Variable;
+
+
+            DIMENSION.Rule = 
+
+            DECLARACION_SIMPLE.Rule = DATATYPE + VARLIST + SEMICOLON
+                | DATATYPE + VARLIST + ASIGNACION + OPER + SEMICOLON;
+
+            DECLARACION_ARRAY.Rule = DATATYPE + ARRAY + VARLIST + DIMENSION + SEMICOLON
+                | DATATYPE + ARRAY + VARLIST + DIMENSION + ASIGNACION + SET_CONTENT + SEMICOLON;
+
+            ASIGNACION_SIMPLE.Rule = Variable + ASIGNACION + OPER + SEMICOLON;
+            ASIGNACION_ARRAY.Rule = Variable + DIMENSION + ASIGNACION + OPER + SEMICOLON;
+            
+            
+
+            #endregion
+            
+
+
+
+            OPER.Rule = OPER + MAS + OPER
+                | OPER + MENOS + OPER
+                | OPER + POR + OPER
+                | OPER + DIVISION + OPER
+                | OPER + POTENCIA + OPER
+                | OPER + IGUAL + OPER
+                | OPER + DIFERENTE + OPER
+                | OPER + MAYOR + OPER
+                | OPER + MENOR + OPER
+                | OPER + MAYOR_IGUAL + OPER
+                | OPER + MENOR_IGUAL + OPER
+                | OPER + OR + OPER
+                | OPER + AND + OPER
+                | NOT + OPER
+                | OPER + INCREMENTO
+                | OPER + DECREMENTO
+                | PARIZQ + OPER + PARDER
+                | Real
+                | Caracter
+                | Cadena
+                | "false"
+                | "true"
+                | "verdadero"
+                | "falso"
+                | Variable
+                | Variable + DIMENSION;
             #endregion
 
             #region Preferencias
@@ -120,15 +183,22 @@ namespace PROYECTO.Gramatica
             NonGrammarTerminals.Add(BlockComment);
             /*-------- PUNTUACIÓN Y AGRUPACIÓN --------*/
             MarkPunctuation(SEMICOLON, COLON, DOT, COMMA, PARIZQ, PARDER, LLVIZQ, LLVDER, CORIZQ, CORDER);
-            /*-------- PUNTUACIÓN Y AGRUPACIÓN --------*/
+            /*-------- ASOCIATIVIDAD --------*/
             RegisterOperators(0, Associativity.Left, OR);
             RegisterOperators(1, Associativity.Left, AND);
             RegisterOperators(2, Associativity.Left, IGUAL, DIFERENTE, MAYOR, MAYOR_IGUAL, MENOR, MENOR_IGUAL);
-            RegisterOperators(3, Associativity.Left, NOT);
+            RegisterOperators(3, Associativity.Right, NOT);
             RegisterOperators(4, Associativity.Left, MAS, MENOS);
             RegisterOperators(5, Associativity.Left, POR, DIVISION);
             RegisterOperators(6, Associativity.Left, POTENCIA);
             RegisterOperators(7, Associativity.Left, PARIZQ, PARDER);
+            /*-------- PALABRAS RESERVADAS --------*/
+            MarkReservedWords(INT.Text, CHAR.Text, STRING.Text, DOUBLE.Text,
+                BOOL.Text, VOID.Text, ARRAY.Text, PUBLICO.Text, PRIVADO.Text,
+                OVERRIDE.Text, IMPORTAR.Text, NEW.Text, MAIN.Text, RETURN.Text,
+                PRINT.Text, SHOW.Text, IF.Text, ELSE.Text, FOR.Text, REPEAT.Text,
+                WHILE.Text, COMPROBAR.Text, CASO.Text, DEFECTO.Text, SALIR.Text,
+                HACER.Text, MIENTRAS.Text, CONTINUAR.Text, "false", "true","verdadero", "falso");
             #endregion
         }
     }
