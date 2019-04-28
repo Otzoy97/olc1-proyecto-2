129ee4d3e5d23e8b861f83de55c50fe5444ada06
otzoy97 <otzoy97@hotmail.com>
2019-04-27 23:34:42 -0600

    Se agregaron algunes clases a utilizar para el manejo de operaciones.

---
+++ PROYECTO/Gramatica/Acciones/Operaciones/IOperacion.cs
@@ -0,0 +1,11 @@
+﻿using PROYECTO.Gramatica.Simbolo;
+using System.Collections.Generic;
+
+
+namespace PROYECTO.Gramatica.Acciones.Operaciones
+{
+    interface IOperacion
+    {
+        void Interpretar(Stack<Symbol> resultado);
+    }
+}
---
+++ PROYECTO/Gramatica/Acciones/Operaciones/Suma.cs
@@ -0,0 +1,13 @@
+﻿using PROYECTO.Gramatica.Simbolo;
+using System.Collections.Generic;
+
+namespace PROYECTO.Gramatica.Acciones.Operaciones
+{
+    class Suma : IOperacion
+    {
+        public void Interpretar(Stack<Symbol> resultado)
+        {
+            
+        }
+    }
+}
