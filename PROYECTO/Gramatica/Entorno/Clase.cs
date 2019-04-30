eb8569d27e05bbe3466a7fc2cdf3e654db492842
otzoy97 <otzoy97@hotmail.com>
2019-04-30 01:20:44 -0600

    Se agregaron comentarios para explicar el propósito de la clase. se comentaron algunos constructores por considerarse innecesarios

--- PROYECTO/Gramatica/Entorno/Clase.cs
+++ PROYECTO/Gramatica/Entorno/Clase.cs
@@ -1,24 +1,64 @@
 ﻿using PROYECTO.Gramatica.Simbolo;
 using System.Collections.Generic;
+using Irony.Parsing;
 
 namespace PROYECTO.Gramatica.Entorno
 {
     class Clase
     { 
-        public Dictionary<string, Funcion> SubEntornos { get; }
-        public Dictionary<string, Symbol> Simbolos { get; }
-
+        /// <summary>
+        /// Almacenará todo los subentorno (funciones/metodos) de la clase
+        /// </summary>
+        public Dictionary<string, Funcion> SubEntornos { get; set; }
+        /// <summary>
+        /// Almacenará todo tipo de dato que se declare (int,string,bool,char,double,clases)
+        /// </summary>
+        public Dictionary<string, Symbol> Simbolos { get; set; }
+        /// <summary>
+        /// Almacenará cualquier otras clases que se hayan importado
+        /// -Esto se especificará en la segunda pasada-
+        /// </summary>
+        public Dictionary<string, Clase> Imports { get; set; }
+        /// <summary>
+        /// Servirá de auxilio para determinar cuales son las clases que se hayan importado
+        /// -Servirá en la primera pasada-
+        /// </summary>
+        public ParseTreeNode SubArbolImports { get; set; }
+        /// <summary>
+        /// Constructor por defecto
+        /// </summary>
         public Clase()
         {
-            SubEntornos = null;
-            Simbolos = null;
+            SubEntornos = new Dictionary<string, Funcion>();
+            Simbolos = new Dictionary<string, Symbol>();
+            Imports = null;
+            SubArbolImports = null;
         }
-
-        public Clase(Dictionary<string, Funcion> subentornos, Dictionary<string, Symbol> simbolos)
+        /// <summary>
+        /// Constructor que acepta un diccionario de fnciones y simbolos
+        /// </summary>
+        /// <param name="subentornos"></param>
+        /// <param name="simbolos"></param>
+        /*public Clase(Dictionary<string, Funcion> subentornos, Dictionary<string, Symbol> simbolos)
         {
             this.SubEntornos = subentornos;
             this.Simbolos = simbolos;
-        }
-        
+            this.Imports = null;
+            this.SubArbolImports = null;
+        }*/
+        /// <summary>
+        /// Construcor que acepta un diccionario de funciones y simbolos
+        /// Además guarda el arbol que especifica los imports para esta clase en particular
+        /// </summary>
+        /// <param name="subentornos"></param>
+        /// <param name="simbolos"></param>
+        /// <param name="subArbol"></param>
+        /*public Clase(Dictionary<string, Funcion> subentornos, Dictionary<string, Symbol> simbolos, ParseTreeNode subArbol )
+        {
+            this.SubEntornos = subentornos;
+            this.Simbolos = simbolos;
+            this.Imports = null;
+            this.SubArbolImports = subArbol;
+        }*/
     }
 }
