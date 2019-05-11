df4f113655c1eb86c5c977f1be8286c9b49edc79
otzoy97 <otzoy97@hotmail.com>
2019-05-10 22:06:05 -0600

    Se agregaron todas las operaciones posibles del compilador y su forma de recorrerlas

---
+++ PROYECTO/Gramatica/Acciones/Operaciones/Operar.cs
@@ -0,0 +1,151 @@
+﻿using Irony.Parsing;
+using PROYECTO.Gramatica.Entorno;
+using System;
+using System.Collections.Generic;
+
+namespace PROYECTO.Gramatica.Acciones.Operaciones
+{
+    class Operar 
+    {
+        public ParseTreeNode OperRaiz { get; }
+
+        public Simbolo Interpretar(ParseTreeNode operRaiz)
+        {
+            Simbolo retorno = new Simbolo();
+            switch (operRaiz.ChildNodes.Count)
+            {
+                case 1:
+                    switch (operRaiz.ChildNodes[0].Term.Name)
+                    {
+                        case "OPER":
+                            retorno = Interpretar(operRaiz.ChildNodes[0]);
+                            break;
+                        case "tkREAL":
+                            //Determina el tipo  (double o int)
+                            if (operRaiz.ChildNodes[0].Token.Value.GetType().Equals(typeof(int)))
+                            {
+                                retorno.TipoDato = Tipo.INT;
+                                retorno.Dato = operRaiz.ChildNodes[0].Token.Value;
+                            }
+                            if (operRaiz.ChildNodes[0].Token.Value.GetType().Equals(typeof(double)))
+                            {
+                                retorno.TipoDato = Tipo.DOUBLE;
+                                retorno.Dato = operRaiz.ChildNodes[0].Token.Value;
+                            }
+                            break;
+                        case "tkCHAR":
+                            retorno.TipoDato = Tipo.CHAR;
+                            retorno.Dato = operRaiz.ChildNodes[0].Token.Value;
+                            break;
+                        case "tkSTR":
+                            retorno.TipoDato = Tipo.STRING;
+                            retorno.Dato = operRaiz.ChildNodes[0].Token.Value;
+                            break;
+                        case "CSTBOOL":
+                            retorno.TipoDato = Tipo.BOOLEAN;
+                            retorno.Dato = operRaiz.ChildNodes[0].Token.Value;
+                            break;
+                        case "tkVAR":
+                            break;
+                        case "CALL":
+                            break;
+                    }
+                    break;
+                case 2:
+                    //Operación unaria
+                    //NEGACION
+                    if (operRaiz.ChildNodes[0].Term.Name.Equals("NOT"))
+                    {
+                        retorno = Interpretar(operRaiz.ChildNodes[1]);
+                        if (retorno.TipoDato == Tipo.BOOLEAN)
+                        {
+                            retorno.Dato = !(bool)retorno.Dato;
+                        }
+                        else
+                        {
+                            Console.WriteLine("La operación 'negación' solo es posible con valores booleanos");
+                        }
+                    }
+                    //INCREMENTO, DECREMENTO
+                    if (operRaiz.ChildNodes[0].Term.Name.Equals("OPER"))
+                    {
+                        retorno = Interpretar(operRaiz.ChildNodes[0]);
+                        //Determina si el tipo de dato es el correcto
+                        if (retorno.TipoDato == Tipo.INT || retorno.TipoDato == Tipo.DOUBLE || retorno.TipoDato == Tipo.CHAR)
+                        {
+                            switch (operRaiz.ChildNodes[0].Term.Name)
+                            {
+                                case "INCREMENTO":
+                                    retorno.Dato = retorno.TipoDato == Tipo.INT || retorno.TipoDato == Tipo.CHAR ? ((int)retorno.Dato) + 1 : ((double)retorno.Dato) + 1;
+                                    retorno.TipoDato = retorno.TipoDato == Tipo.INT || retorno.TipoDato == Tipo.CHAR ? Tipo.INT : Tipo.DOUBLE;
+                                    break;
+                                case "DECREMENTO":
+                                    retorno.Dato = retorno.TipoDato == Tipo.INT || retorno.TipoDato == Tipo.CHAR ? ((int)retorno.Dato) - 1 : ((double)retorno.Dato) - 1;
+                                    retorno.TipoDato = retorno.TipoDato == Tipo.INT || retorno.TipoDato == Tipo.CHAR ? Tipo.INT : Tipo.DOUBLE;
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("La operación 'incremento/decremento' solo es posible con int, double o char");
+                        }
+
+                    }
+                    //DECLARACION DE CLASE
+                    if (operRaiz.ChildNodes[0].Term.Name.Equals("NEW"))
+                    {
+                        retorno.TipoDato = Tipo.CLASE;
+                        string nombreClase = operRaiz.ChildNodes[1].Token.Text.ToLower();
+                        //Busca en la lista de clases si existe una clases con el nombre anterior
+                        //Clona la clase en cuestión y la asigna a retorno.Dato
+                    }
+                    //Variables
+                    if (operRaiz.ChildNodes[0].Term.Name.Equals("tkVAR"))
+                    {
+                        string nombreVariable = operRaiz.ChildNodes[1].Token.Text.ToLower();
+                        //Busca en la lista de simbolos, verifica que exista el nombre de la variable
+                        //Determina si es un array
+                        //Determina las dimensiones del array
+                        //Devuelve el simbolo
+                    }
+                    break;
+                case 3:
+                    //Operación binaria
+                    switch (operRaiz.ChildNodes[1].Term.Name)
+                    {
+                        case "MAS":
+
+                            break;
+                        case "MENOS":
+                            break;
+                        case "POR":
+                            break;
+                        case "DIVISION":
+                            break;
+                        case "POTENCIA":
+                            break;
+                        case "IGUAL":
+                            break;
+                        case "DIFERENTE":
+                            break;
+                        case "MAYOR":
+                            break;
+                        case "MENOR":
+                            break;
+                        case "MAYOR_IGUAL":
+                            break;
+                        case "MENOR_IGUAL":
+                            break;
+                        case "DOT":
+                            break;
+                        case "OR":
+                            break;
+                        case "AND":
+                            break;
+                    }
+                    break;
+            }
+            return retorno;
+        }
+    }
+}
--- PROYECTO/Gramatica/Acciones/Operaciones/Suma.cs
+++ PROYECTO/Gramatica/Acciones/Operaciones/Suma.cs
@@ -1,13 +1,229 @@
-﻿using PROYECTO.Gramatica.Simbolo;
+﻿using Irony.Parsing;
+using PROYECTO.Gramatica.Entorno;
+using System;
 using System.Collections.Generic;
+using System.Data;
 
 namespace PROYECTO.Gramatica.Acciones.Operaciones
 {
-    class Suma : IOperacion
+    class Suma //: IOperacion
     {
-        public void Interpretar(Stack<Symbol> resultado)
+        /// <summary>
+        /// Devuelve un simbolo
+        /// </summary>
+        /// <param name="raiz"></param>
+        /// <param name="operClass"></param>
+        /// <returns></returns>
+        public Simbolo Interpretar(ParseTreeNode raiz, Operar operClass)
         {
-            
+            Simbolo symizq = operClass.Interpretar(raiz.ChildNodes[0]);
+            Simbolo symder = operClass.Interpretar(raiz.ChildNodes[2]);
+            //ENTERO
+            if (symizq.TipoDato == Tipo.INT)
+            {
+                return SumarInt((int)symizq.Dato, symder);
+            }
+            if (symder.TipoDato == Tipo.INT)
+            {
+                return SumarInt((int)symder.Dato, symizq);
+            }
+            //STRING
+            if (symizq.TipoDato == Tipo.STRING)
+            {
+                return SumarString(symizq.Dato.ToString(), symder);
+            }
+            if (symder.TipoDato == Tipo.STRING)
+            {
+                return SumarString(symder.Dato.ToString(), symizq);
+            }
+            //DOUBLE
+            if (symizq.TipoDato == Tipo.DOUBLE)
+            {
+                return SumarDouble((double)symizq.Dato, symder);
+            }
+            if (symder.TipoDato == Tipo.DOUBLE)
+            {
+                return SumarDouble((double)symder.Dato, symizq);
+            }
+            //CHAR
+            if (symizq.TipoDato == Tipo.CHAR)
+            {
+                return SumarChar((char)symizq.Dato, symder);
+            }
+            if (symder.TipoDato == Tipo.CHAR)
+            {
+                return SumarChar((char)symder.Dato, symizq);
+            }
+            //BOOLEANO
+            if (symizq.TipoDato == Tipo.BOOLEAN)
+            {
+                return SumarBool((bool)symizq.Dato, symder);
+            }
+            if (symder.TipoDato == Tipo.BOOLEAN)
+            {
+                return SumarBool((bool)symder.Dato, symizq);
+            }
+            return new Simbolo();
+        }
+        /// <summary>
+        /// Maneja la forma de sumar entre un entero y el resto de tipos primitivos
+        /// </summary>
+        /// <param name="intVar"></param>
+        /// <param name="sym"></param>
+        /// <returns></returns>
+        public Simbolo SumarInt(int intVar, Simbolo sym)
+        {
+            Simbolo retorno = new Simbolo();
+            switch (sym.TipoDato)
+            {
+                case Tipo.INT:
+                    retorno.Dato = intVar + ((int)sym.Dato);
+                    retorno.TipoDato = Tipo.INT;
+                    break;
+                case Tipo.STRING:
+                    retorno.Dato = intVar.ToString() + sym.Dato.ToString();
+                    retorno.TipoDato = Tipo.STRING;
+                    break;
+                case Tipo.CHAR:
+                    retorno.Dato = intVar + ((char)sym.Dato);
+                    retorno.TipoDato = Tipo.INT;
+                    break;
+                case Tipo.DOUBLE:
+                    retorno.Dato = intVar + ((double)sym.Dato);
+                    retorno.TipoDato = Tipo.DOUBLE;
+                    break;
+                case Tipo.BOOLEAN:
+                    retorno.Dato = intVar + ((bool)sym.Dato ? 1 : 0);
+                    retorno.TipoDato = Tipo.INT;
+                    break;
+            }
+            return retorno;
+        }
+        /// <summary>
+        /// Maneja la forma de sumar entre un char y el resto de tipos primitivos
+        /// </summary>
+        /// <param name="intVar"></param>
+        /// <param name="sym"></param>
+        /// <returns></returns>
+        public Simbolo SumarChar(char intVar, Simbolo sym)
+        {
+            Simbolo retorno = new Simbolo();
+            switch (sym.TipoDato)
+            {
+                case Tipo.INT:
+                    retorno.Dato = intVar + ((int)sym.Dato);
+                    retorno.TipoDato = Tipo.INT;
+                    break;
+                case Tipo.STRING:
+                    retorno.Dato = intVar.ToString() + sym.Dato.ToString();
+                    retorno.TipoDato = Tipo.STRING;
+                    break;
+                case Tipo.CHAR:
+                    retorno.Dato = intVar + ((char)sym.Dato);
+                    retorno.TipoDato = Tipo.INT;
+                    break;
+                case Tipo.DOUBLE:
+                    retorno.Dato = intVar + ((double)sym.Dato);
+                    retorno.TipoDato = Tipo.DOUBLE;
+                    break;
+                case Tipo.BOOLEAN:
+                    retorno.Dato = intVar + ((bool)sym.Dato ? 1 : 0);
+                    retorno.TipoDato = Tipo.INT;
+                    break;
+            }
+            return retorno;
+        }
+        /// <summary>
+        /// Maneja la forma de sumar entre un string y el resto de tipos primitivos
+        /// </summary>
+        /// <param name="strVar"></param>
+        /// <param name="sym"></param>
+        /// <returns></returns>
+        public Simbolo SumarString(String strVar, Simbolo sym)
+        {
+            Simbolo retorno = new Simbolo();
+            switch (sym.TipoDato)
+            {
+                case Tipo.INT:
+                case Tipo.STRING:
+                case Tipo.DOUBLE:
+                case Tipo.CHAR:
+                    retorno.Dato = strVar + sym.Dato.ToString();
+                    retorno.TipoDato = Tipo.STRING;
+                    break;
+                case Tipo.BOOLEAN:
+                    Console.WriteLine("No se puede sumar una cadena y un booleano");
+                    break;
+            }
+            return retorno;
+        }
+        /// <summary>
+        /// Maneja la forma de sumar entre un double y el resto de tipos primitivos
+        /// </summary>
+        /// <param name="intVar"></param>
+        /// <param name="sym"></param>
+        /// <returns></returns>
+        public Simbolo SumarDouble(double intVar, Simbolo sym)
+        {
+            Simbolo retorno = new Simbolo();
+            switch (sym.TipoDato)
+            {
+                case Tipo.INT:
+                    retorno.Dato = intVar + ((int)sym.Dato);
+                    retorno.TipoDato = Tipo.DOUBLE;
+                    break;
+
+                case Tipo.STRING:
+                    retorno.Dato = intVar.ToString() + sym.Dato.ToString();
+                    retorno.TipoDato = Tipo.STRING;
+                    break;
+                case Tipo.DOUBLE:
+                    retorno.Dato = intVar + ((double)sym.Dato);
+                    retorno.TipoDato = Tipo.DOUBLE;
+                    break;
+                case Tipo.CHAR:
+                    retorno.Dato = intVar + ((char)sym.Dato);
+                    retorno.TipoDato = Tipo.DOUBLE;
+                    break;
+                case Tipo.BOOLEAN:
+                    retorno.Dato = intVar + ((bool)sym.Dato ? 1 : 0);
+                    retorno.TipoDato = Tipo.DOUBLE;
+                    break;
+            }
+            return retorno;
+        }
+        /// <summary>
+        /// Maneja la forma de sumar entre un booleano y el resto de tipos primitivos
+        /// </summary>
+        /// <param name="boolVar"></param>
+        /// <param name="sym"></param>
+        /// <returns></returns>
+        public Simbolo SumarBool(bool boolVar, Simbolo sym)
+        {
+            Simbolo retorno = new Simbolo();
+            switch (sym.TipoDato)
+            {
+                case Tipo.INT:
+                    retorno.Dato = (boolVar ? 1 : 0) + ((int)sym.Dato);
+                    retorno.TipoDato = Tipo.INT;
+                    break;
+                case Tipo.STRING:
+                    Console.WriteLine("No se puede sumar una cadena y un booleano");
+                    break;
+                case Tipo.DOUBLE:
+                    retorno.Dato = (boolVar ? 1 : 0) + ((double)sym.Dato);
+                    retorno.TipoDato = Tipo.DOUBLE;
+                    break;
+                case Tipo.CHAR:
+                    retorno.Dato = (boolVar ? 1 : 0) + ((char)sym.Dato);
+                    retorno.TipoDato = Tipo.INT;
+                    break;
+                case Tipo.BOOLEAN:
+                    retorno.Dato = boolVar || (bool)sym.Dato;
+                    retorno.TipoDato = Tipo.BOOLEAN;
+                    break;
+            }
+            return retorno;
         }
     }
 }
