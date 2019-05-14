e69637559035342e0d3c3ec5ddb17cdb1e201241
otzoy97 <otzoy97@hotmail.com>
2019-05-13 23:33:56 -0600

    - Se agregó el método para copiar clases - Se modificó la forma de almacenar las clases importadas , ahora es una lista con nombre de las clases a importar

--- PROYECTO/Gramatica/Entorno/Clase.cs
+++ PROYECTO/Gramatica/Entorno/Clase.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using Irony.Parsing;
+﻿using System;
+using System.Collections.Generic;
 
 namespace PROYECTO.Gramatica.Entorno
 {
@@ -19,10 +19,10 @@ namespace PROYECTO.Gramatica.Entorno
         /// </summary>
         public Dictionary<string, Clase> ClaseImp { get; set; }
         /// <summary>
-        /// Servirá de auxilio para determinar cuales son las clases que se hayan importado
-        /// -Servirá en la primera pasada-
+        /// Lista con los nombre de las clases que se deberán buscar en la
+        /// lista de clases
         /// </summary>
-        public ParseTreeNode ClaseImpTree { get; set; }
+        public LinkedList<String> ClaseImpNames { get; set; }
         /// <summary>
         /// Constructor por defecto
         /// </summary>
@@ -30,8 +30,8 @@ namespace PROYECTO.Gramatica.Entorno
         {
             ClaseEnt = new Dictionary<string, Funcion>();
             ClaseSym = new Dictionary<string, Simbolo>();
-            ClaseImp = null;
-            ClaseImpTree = null;
+            ClaseImp = new Dictionary<string, Clase>();
+            ClaseImpNames = new LinkedList<string>();
         }
 
 
@@ -48,5 +48,92 @@ namespace PROYECTO.Gramatica.Entorno
 
         }
 
+        /// <summary>
+        /// Busca en el diccionario de Funciones de la clase y en las funciones de las clases importadas
+        /// </summary>
+        /// <returns></returns>
+        public Funcion BuscarFuncion(string nombreFuncion)
+        {
+            ///Busca en el diccionario local
+            if (ClaseEnt.ContainsKey(nombreFuncion))
+            {
+                return ClaseEnt[nombreFuncion];
+            }
+            //Recorre el diccionario de clases importadas
+            //en busca de una coincidencia
+            foreach (var flagClass in ClaseImp)
+            {
+                //Utiliza recursion para buscar la función
+                Funcion ret = flagClass.Value.BuscarFuncion(nombreFuncion);
+                //Verifica que no sea nulo lo que se devolvió
+                if (ret != null)
+                {
+                    //Si no es nulo retorna el valor
+                    return ret;
+                }
+            }
+            //No encontró ninguna coincidencia, retorna un null
+            return null;
+        }
+        /// <summary>
+        /// Realiza una copia de la clase específicada
+        /// </summary>
+        /// <param name="original">Clase a copiar</param>
+        /// <returns></returns>
+        public static Clase Copiar(Clase original)
+        {
+            Clase prueba = new Clase();
+            ///Copia los simbolos
+            foreach (var lst01 in original.ClaseSym)
+            {
+                Simbolo symPrueba = new Simbolo
+                {
+                    Arr = lst01.Value.Arr,
+                    Dato = lst01.Value.Dato,
+                    EsPrivado = lst01.Value.EsPrivado,
+                    Oper = lst01.Value.Oper,
+                    TipoDato = lst01.Value.TipoDato
+                };
+
+                if (lst01.Value.Arreglo != null)
+                {
+                    Arreglo arr = new Arreglo
+                    {
+                        Dimension = lst01.Value.Arreglo.Dimension,
+                        SizeBi = lst01.Value.Arreglo.SizeBi,
+                        SizeUni = lst01.Value.Arreglo.SizeUni,
+                        SizeTri = lst01.Value.Arreglo.SizeTri
+                    };
+                    symPrueba.Arreglo = arr;
+                }
+                if (lst01.Value.Posicion != null)
+                {
+                    Posicion pos = new Posicion(lst01.Value.Posicion.Fila, lst01.Value.Posicion.Columna);
+                    symPrueba.Posicion = pos;
+                }
+                prueba.ClaseSym.Add(lst01.Key.ToString(), symPrueba);
+            }
+            ///Copia los nombre de las clases importadas
+            foreach (var lst01 in original.ClaseImpNames)
+            {
+                prueba.ClaseImpNames.AddLast(lst01.ToString());
+            }
+            ///Copia las funciones de la clase
+            foreach (var lst01 in original.ClaseEnt)
+            {
+                Funcion funcion = Funcion.Copiar(lst01.Value);
+                funcion.ClaseEnt = prueba.ClaseEnt;
+                funcion.ClaseImp = prueba.ClaseImp;
+                funcion.ClaseImpNames = prueba.ClaseImpNames;
+                funcion.ClaseSym = prueba.ClaseSym;
+                prueba.ClaseEnt.Add(lst01.Key, funcion);
+            }
+            ///Copia las clases importadas
+            foreach (var lst01 in original.ClaseImp)
+            {
+                prueba.ClaseImp.Add(lst01.Key.ToString(), Copiar(lst01.Value));
+            }
+            return prueba;
+        }
     }
 }
