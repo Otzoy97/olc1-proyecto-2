877f197754e8a847a68c3d0386d195562c77d8cb
otzoy97 <otzoy97@hotmail.com>
2019-05-14 16:05:33 -0600

    Se agregó la clase que controlará las nativas del programa

---
+++ PROYECTO/Gramatica/Acciones/Nativa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROYECTO.Gramatica.Acciones
+{
+    /// <summary>
+    /// Maneja todas las funciones nativas del programa
+    /// </summary>
+    static class Nativa
+    {
+        /// <summary>
+        /// Imprime un mensaje en la consola de salida
+        /// </summary>
+        /// <param name="mensaje"></param>
+        public static void Print(string mensaje)
+        {
+            Main.TxtOutput.Text += String.Format("{0}{1}", mensaje, Environment.NewLine);
+        }
+        /// <summary>
+        /// Despliega una ventana modal
+        /// </summary>
+        /// <param name="titulo">El titulo que se mostrará en el modal</param>
+        /// <param name="mensaje">El mensaje que se mostrará en el modal</param>
+        public static void Show(object titulo, object mensaje)
+        {
+            MessageBox.Show(Main.TxtOutput.TopLevelControl, mensaje.ToString(), titulo.ToString());
+        }
+    }
+}
